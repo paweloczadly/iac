b5423b3d9bd6dcbc55cb7c496c15ad7827c66719
Paweł Oczadły <p@oczadly.io>
2025-07-03 20:21:17 +0200

    feat(repo/github): add oczadly.io repository provisioning

---
+++ repositories/github/oczadly-io/main.tf
@@ -0,0 +1,36 @@
+resource "github_repository" "repo" {
+  name         = "oczadly.io"
+  description  = "Blog and personal website for Paweł Oczadły."
+  visibility   = "public"
+  homepage_url = "https://oczadly.io"
+
+  auto_init    = true
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  license_template = "mit"
+
+  pages {
+    source {
+      branch = "main"
+      path   = "/"
+    }
+  }
+}
+
+resource "github_branch" "branch_main" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
+
+resource "github_branch" "branch_develop" {
+  repository = github_repository.repo.name
+  branch     = "develop"
+}
+
+resource "github_branch_default" "repo_default_branch" {
+  repository = github_repository.repo.name
+  branch     = github_branch.branch_develop.branch
+}
