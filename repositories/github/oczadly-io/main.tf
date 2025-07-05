5b8edde7648f857fb2822e42b48f6dc0452643e0
Paweł Oczadły <p@oczadly.io>
2025-07-05 18:02:23 +0200

    feat(repo/github): create oczadly.io repository

---
+++ repositories/github/oczadly-io/main.tf
@@ -0,0 +1,41 @@
+resource "github_repository" "repo" {
+  name         = "oczadly.io"
+  description  = "Blog and personal website for Paweł Oczadły."
+  visibility   = "public"
+  homepage_url = "https://oczadly.io"
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+
+  delete_branch_on_merge = true
+
+  template {
+    owner      = "paweloczadly"
+    repository = "blog-template"
+    include_all_branches = true
+  }
+
+  lifecycle {
+    ignore_changes = [pages]
+  }
+}
+
+resource "github_branch_protection" "branch_protection_main" {
+  repository_id = github_repository.repo.id
+  pattern       = "main"
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+
+  required_status_checks {
+    contexts = []
+    strict   = true
+  }
+}
+
+resource "github_actions_repository_permissions" "write_permissions" {
+  repository      = github_repository.repo.name
+  allowed_actions = "all"
+}
