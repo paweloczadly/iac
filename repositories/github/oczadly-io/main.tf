7cfd6a9ea6f4e6b3efba4fd339e3dfb49bd831de
Paweł Oczadły <p@oczadly.io>
2025-07-05 13:44:52 +0200

    feat: remove oczadly.io repo as DR excercise (#5)

--- repositories/github/oczadly-io/main.tf
+++ repositories/github/oczadly-io/main.tf
@@ -4,30 +4,39 @@ resource "github_repository" "repo" {
   visibility   = "public"
   homepage_url = "https://oczadly.io"
 
-  auto_init    = true
   has_issues   = true
   has_wiki     = false
   has_projects = false
-  is_template  = false
 
-  license_template = "mit"
+  delete_branch_on_merge = true
 
-  pages {
-    cname = "oczadly.io"
+  template {
+    owner      = "paweloczadly"
+    repository = "blog-template"
+    # include_all_branches = true
+    include_all_branches = false // TODO: change me
+  }
 
-    source {
-      branch = "gh-pages"
-      path   = "/"
-    }
+  lifecycle {
+    ignore_changes = [pages]
   }
 }
 
-resource "github_branch" "branch_main" {
-  repository = github_repository.repo.name
-  branch     = "main"
+resource "github_branch_protection" "branch_protection_main" {
+  repository_id = github_repository.repo.id
+  pattern       = "main"
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+
+  required_status_checks {
+    contexts = []
+    strict   = true
+  }
 }
 
-resource "github_branch_default" "repo_default_branch" {
-  repository = github_repository.repo.name
-  branch     = github_branch.branch_main.branch
+resource "github_actions_repository_permissions" "write_permissions" {
+  repository      = github_repository.repo.name
+  allowed_actions = "all"
 }
