42d36005fbb8819cdedee868b09b408d73efa7ab
Paweł Oczadły <p@oczadly.io>
2025-08-26 07:40:39 +0200

    feat(github): create infra-at-scale/.github repository (#14)

---
+++ repositories/github/infra-at-scale/.github/main.tf
@@ -0,0 +1,37 @@
+resource "github_repository" "repo" {
+  name        = ".github"
+  description = "Organization profile repository"
+  visibility  = "public"
+
+  auto_init        = true
+  license_template = "mit"
+
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+
+  vulnerability_alerts = true
+}
+
+resource "github_branch" "branch_main" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "repo_default_branch" {
+  repository = github_repository.repo.name
+  branch     = github_branch.branch_main.branch
+}
+
+resource "github_repository_file" "readme_md" {
+  repository          = github_repository.repo.name
+  branch              = github_branch.branch_main.branch
+  file                = "profile/README.md"
+  content             = file("${path.module}/files/README.md")
+  commit_message      = "feat(README): change profile README.md"
+  commit_author       = "Paweł Oczadły"
+  commit_email        = "p@oczadly.io"
+  overwrite_on_create = true
+}
