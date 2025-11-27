c120481b8008e08b3c70a387c76bf9e4e053ebaf
Paweł Oczadły <p@oczadly.io>
2025-11-27 08:11:07 +0100

    feat(repo/github): create renovate-config repository

---
+++ repositories/github/paweloczadly/renovate-config/main.tf
@@ -0,0 +1,42 @@
+resource "github_repository" "repo" {
+  name        = "renovate-config"
+  description = "Shared Renovate configuration with weekly automated dependency updates for paweloczadly and infra-at-scale projects."
+  visibility  = "public"
+
+  auto_init = false
+
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+}
+
+resource "github_branch" "branch_main" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "repo_default_branch" {
+  repository = github_repository.repo.name
+  branch     = github_branch.branch_main.branch
+}
+
+resource "github_branch_protection" "branch_protection_main" {
+  repository_id = github_repository.repo.id
+  pattern       = "main"
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+
+  required_status_checks {
+    contexts = []
+    strict   = true
+  }
+}
+
+resource "github_actions_repository_permissions" "write_permissions" {
+  repository      = github_repository.repo.name
+  allowed_actions = "all"
+}
